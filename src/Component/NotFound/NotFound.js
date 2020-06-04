4e786ea2e221c7997183eec563e52684f2595d06
mdmaruf43 <marufahmedcse76@gmail.com>
2020-06-04 18:31:19 +0600

    NotFound component added

---
+++ src/Component/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NotFound.css'
+import {
+    Link
+} from "react-router-dom";
+
+function NotFound() {
+    return (
+        <div id="bcImg" className="d-flex justify-content-center align-items-end">
+            <Link to="/">
+                <h3>Back to Homepage</h3>
+            </Link>
+            
+        </div>
+    )
+}
+
+export default NotFound
