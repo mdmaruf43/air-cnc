47340ff7e5d071c618acd085903386b6f4b5b414
Shakil Ahmed <shakilatrai5@gmail.com>
2020-06-05 21:30:14 +0600

    Apartment details page completed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import {
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import ApartDetails from './components/ApartDetails/ApartDetails';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/home">
-
+        <Navbar></Navbar>
+        <ApartDetails></ApartDetails>
         </Route>
         <Route path="/users">
         </Route>
