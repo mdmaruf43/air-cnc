7d2bc68a3c7a4b49a6f798e7110671c634774c71
mdmaruf43 <marufahmedcse76@gmail.com>
2020-06-05 12:52:12 +0600

    3 page done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import {
   Route
 } from "react-router-dom";
 import NotFound from './Component/NotFound/NotFound';
+import ReviewHouse from './Component/ReviewHouse/ReviewHouse';
+import Coming from './Component/Coming/Coming';
+import Confirm from './Component/Confirm/Confirm';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <NotFound />
+          <ReviewHouse />
+        </Route>
+        <Route path="/coming">
+          <Coming />
+        </Route>
+        <Route path="/confirm">
+          <Confirm />
         </Route>
         <Route path="*">
           <NotFound/>
--- src/Component/Confirm/Confirm.js
+++ src/Component/Confirm/Confirm.js
@@ -1,72 +1,27 @@
 import React from 'react';
-import './ReviewHouse.css';
-import { BsFillHouseFill } from "react-icons/bs";
-import { MdSmokingRooms } from "react-icons/md";
-import { MdDoNotDisturb } from "react-icons/md";
-import { MdPets } from "react-icons/md";
+import './Confirm.css';
 import { BsChevronCompactDown } from "react-icons/bs";
 import { AiFillStar } from "react-icons/ai";
 import cardImg from '../../Images/cardImg.jpg'
 import { BsArrowRightShort } from "react-icons/bs";
 import { IoIosArrowForward } from "react-icons/io";
+import Payment from '../Payment/Payment'
 
-function ReviewHouse() {
+function Confirm() {
     return (
         <div className="container pt-2">
             <div className="d-flex">
                 <p className="fontSize1">1. Reviews house rules <span className="fontSize"> <IoIosArrowForward /> </span></p>
-                <p className="textColor">2. Who's coming? <span><IoIosArrowForward /> </span></p>
-                <p className="textColor">3. Confirm and pay </p>
+                <p className="fontSize1">2. Who's coming? <span><IoIosArrowForward /> </span></p>
+                <p className="fontSize1">3. Confirm and pay </p>
             </div>
-            <div className="row">
-                <div className="col-md-6 col-sm-12">
-                    <h3>Review house rules</h3>
-                    <h5 className="pt-2">3 nights in Dhaka</h5>
-                    <div className="d-flex justify-content-between pt-2">
-                        <div className="d-flex">
-                            <div className="mr-2">
-                                <p className="bgColor">APR <br/> 13</p> 
-                            </div>
-                            <div>
-                                <p>Monday check-in <br/> After 12:00 PM</p>
-                            </div>
-                        </div>
-                        <div className="d-flex">
-                            <div className="mr-2">
-                                <p className="bgColor">APR <br/> 17</p> 
-                            </div>
-                            <div>
-                                <p>Thursday checkout <br/> 11:00 AM</p>
-                            </div>
-                        </div>
-                    </div>
-                    <p>Self check-in with building staff</p>
-                    <div className="borderBottom"></div>
-                    <div className="pt-2">
-                        <h5>Things to keep in mind</h5>
-                        <div className="pt-2">
-                            <div className="d-flex">
-                                <p className="mr-3 border p-2"><BsFillHouseFill /> </p>
-                                <p className="pt-2">Sultable for children and infants</p>
-                            </div>
-                            <div className="d-flex">
-                                <p className="mr-3 border p-2"><MdPets /> </p>
-                                <p className="pt-2">Pets allowed</p>
-                            </div>
-                            <div className="d-flex">
-                                <p className="mr-3 border p-2"><MdDoNotDisturb /> </p>
-                                <p className="pt-2">No parties or events</p>
-                            </div>
-                            <div className="d-flex">
-                                <p className="mr-3 border p-2"><MdSmokingRooms /> </p>
-                                <p className="pt-2">Smoking allowed</p>
-                            </div>
-                            <p className="text-primary">Read more <span className="ml-2"><BsChevronCompactDown /></span> </p>
-                        </div>
-                        <button className="btn1"> <a style={{textDecoration: 'none', color: '#fff'}} href="/coming">Agree and continue</a> </button>
-                    </div>
+            <div className="row pt-3">
+                <div className="col-md-7 col-sm-12 pt-5">
+                    <h3>Payment Selection</h3>
+                    <Payment />
                 </div>
-                <div className="col-md-6 col-sm-12">
+                <div className="col-md-5 col-sm-12">
+                    <div className="pt-2"></div>
                     <div className="card boxShadow">
                         <div className="card-body">
                             <div className="d-flex justify-content-between">
@@ -129,4 +84,4 @@ function ReviewHouse() {
     )
 }
 
-export default ReviewHouse
+export default Confirm
